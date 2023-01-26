2eccd4b00660f31774de048c7993bdaff900c95b
pmmaria <mary.perez.m@gmail.com>
2023-01-27 00:12:10 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './index.css'
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import RouterApp from './Components/RouterApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <Router>
+  <HashRouter>
     <RouterApp />
-  </Router>
+  </HashRouter>
   // </React.StrictMode>
 );

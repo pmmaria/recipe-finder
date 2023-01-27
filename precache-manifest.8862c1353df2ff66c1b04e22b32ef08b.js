34de4b2470cfbc4245463eb8ca5a4051585864a4
pmmaria <mary.perez.m@gmail.com>
2023-01-27 01:03:07 +0100

    Updates

---
+++ precache-manifest.8862c1353df2ff66c1b04e22b32ef08b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "361a12dd327c4024cae7",
+    "url": "/recipe-finder/static/css/main.99d4fae6.chunk.css"
+  },
+  {
+    "revision": "361a12dd327c4024cae7",
+    "url": "/recipe-finder/static/js/main.361a12dd.chunk.js"
+  },
+  {
+    "revision": "201250f176f1d945eb12",
+    "url": "/recipe-finder/static/css/1.c0bb6864.chunk.css"
+  },
+  {
+    "revision": "201250f176f1d945eb12",
+    "url": "/recipe-finder/static/js/1.201250f1.chunk.js"
+  },
+  {
+    "revision": "693457eb8e1e72c33441",
+    "url": "/recipe-finder/static/js/runtime~main.693457eb.js"
+  },
+  {
+    "revision": "40e6df55734232692408d76ad5f9c34a",
+    "url": "/recipe-finder/static/media/fa-regular-400.40e6df55.ttf"
+  },
+  {
+    "revision": "4b162098158528431aeb5636116777f0",
+    "url": "/recipe-finder/static/media/fa-regular-400.4b162098.woff2"
+  },
+  {
+    "revision": "e2f5b365c7d3d4497da73148ddfae997",
+    "url": "/recipe-finder/static/media/fa-brands-400.e2f5b365.woff2"
+  },
+  {
+    "revision": "0ef11ce096706bcafc520aee12162ae4",
+    "url": "/recipe-finder/static/media/fa-brands-400.0ef11ce0.ttf"
+  },
+  {
+    "revision": "328a9d0f59f0ebb55cddac6f39995bea",
+    "url": "/recipe-finder/static/media/fa-solid-900.328a9d0f.woff2"
+  },
+  {
+    "revision": "36818ecc50af8ea6c4cbef88d0b2baad",
+    "url": "/recipe-finder/static/media/fa-v4compatibility.36818ecc.ttf"
+  },
+  {
+    "revision": "c0bf087a238488d351270a35c8f0cca0",
+    "url": "/recipe-finder/static/media/fa-solid-900.c0bf087a.ttf"
+  },
+  {
+    "revision": "6ddaf31a1e0e05490f6919cbb3347e9d",
+    "url": "/recipe-finder/static/media/fa-v4compatibility.6ddaf31a.woff2"
+  },
+  {
+    "revision": "c5b45d5f5473b15b5afc9112382ea0f8",
+    "url": "/recipe-finder/static/media/hero-bg.c5b45d5f.jpg"
+  },
+  {
+    "revision": "200cdbb624a0e50fdf7e45682edad1bf",
+    "url": "/recipe-finder/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/recipe-finder/precache-manifest.8862c1353df2ff66c1b04e22b32ef08b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/recipe-finder/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
